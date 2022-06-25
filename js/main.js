9d5b823af525bba88c6be9029dfdb580ebd1143e
StafWeb <staf.target@yandex.ru>
2022-06-25 22:32:36 +0700

    fix mobile 2

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ const swiper = new Swiper('.swiper', {
   // parallax: true,
   // centeredSlides: true,
   slidesPerView: 3,
-  spaceBetween: 60,
+  spaceBetween: 30,
   // centeredSlidesBounds:true,
   passiveListeners: true,
   mousewheel: {
